0ada1a562b7838752b2e9675cf716f601b452061
s24729-pj <s24729@pjwstk.edu.pl>
2024-01-31 17:28:05 +0100

    Initial commit

---
+++ src/main/java/org/example/jazrentalservice/JazRentalServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.jazrentalservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazRentalServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JazRentalServiceApplication.class, args);
+    }
+
+}
