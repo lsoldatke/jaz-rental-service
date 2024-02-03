3262bd91f5076da28e8a51b7749a65b78094184a
s24729-pj <s24729@pjwstk.edu.pl>
2024-02-03 15:31:10 +0100

    Added service

---
+++ src/main/java/org/example/jazrentalservice/service/RentalService.java
@@ -0,0 +1,27 @@
+package org.example.jazrentalservice.service;
+
+import org.example.jazrentalservice.model.Movie;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class RentalService {
+    private final RestTemplate restTemplate;
+
+    public RentalService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public Movie getMovie(Long id) {
+        String url = "http://localhost:8080/movieservice/movies/" + id;
+        ResponseEntity<Movie> response = restTemplate.getForEntity(url, Movie.class);
+        return response.getBody();
+    }
+
+    public Movie returnMovie(Long id) {
+        String url = "http://localhost:8080/movieservice/movies/return/" + id;
+        ResponseEntity<Movie> response = restTemplate.getForEntity(url, Movie.class);
+        return response.getBody();
+    }
+}
