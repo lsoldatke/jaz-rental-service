b0bbc5ff9262fa6934ab3d8855ef95bc5b9dbcf6
s24729-pj <s24729@pjwstk.edu.pl>
2024-02-03 15:31:26 +0100

    Added REST controller

---
+++ src/main/java/org/example/jazrentalservice/controller/RentalServiceRestController.java
@@ -0,0 +1,39 @@
+package org.example.jazrentalservice.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.example.jazrentalservice.model.Movie;
+import org.example.jazrentalservice.service.RentalService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.NoSuchElementException;
+
+@RestController
+@RequestMapping("/rentalservice")
+public class RentalServiceRestController {
+    private final RentalService rentalService;
+
+    public RentalServiceRestController(RentalService rentalService) {
+        this.rentalService = rentalService;
+    }
+
+    @GetMapping("/getmovie/{id}")
+    public Movie getMovie(@PathVariable Long id) {
+        Movie movie = rentalService.getMovie(id);
+        if (movie == null) {
+            throw new NoSuchElementException("Movie not found");
+        }
+        return movie;
+    }
+
+    @Operation(summary = "Return movie by ID", description = "Returns movie with given ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved"),
+            @ApiResponse(responseCode = "404", description = "Movie not found")
+    })
+    @PutMapping("/returnmovie/{id}")
+    public Movie returnMovie(@PathVariable Long id) {
+        return rentalService.returnMovie(id);
+    }
+}
